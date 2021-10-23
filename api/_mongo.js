6411e6da71fdca9626672dd62dff7c2bcd61191b
arnu515 <arnu5152@gmail.com>
2021-10-23 09:43:28 +0530

    use commonjs instead os esmodule

--- api/_mongo.js
+++ api/_mongo.js
@@ -1,6 +1,5 @@
-import { MongoClient } from "mongodb";
-
+const { MongoClient } = require("mongodb");
 const mongo = new MongoClient(process.env.MONGODB_URL);
 
 // Export the connection promise
-export default mongo.connect();
+module.exports = mongo.connect();
